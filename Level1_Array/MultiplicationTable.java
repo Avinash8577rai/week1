242ab89196e2786b6797d537e709e719e2aece60
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-01-10 15:59:32 +0530

    calculate the multiplication with 4 arrays problem5

---
+++ Level1_Array/MultiplicationTable.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class MultiplicationTable{
+    public static void main(String[] args) {
+	
+	// define the object input 
+        Scanner input = new Scanner(System.in);
+        
+      // take user input for thr number
+	  System.out.println(" Enter a number for which you want the multiplication table " );
+		 int number= input.nextInt();
+		 
+		 //define an array to store multiplication result
+		 int[] multiplicationResult = new int[4];
+		 
+		// Generate multiplication table for numbers 6 to 9
+        for (int i = 0; i < multiplicationResult.length; i++) {
+            int multiplier = i + 6; // Multiplier starts from 6
+            multiplicationResult[i] = number * multiplier;
+        }
+
+        // Display the results
+        System.out.println("Multiplication Table:");
+        for (int i = 0; i < multiplicationResult.length; i++) {
+            int multiplier = i + 6;
+            System.out.println(number + " * " + multiplier + " = " + multiplicationResult[i]);
+        }
+
+
+        // Close the input objecty
+	
+        input.close();
+    }
+}
