509293352047b4160330ec6aaab7595258fee28b
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-01-10 09:45:49 +0530

    DistanceConversion_Problem3

---
+++ level1/DistanceConversion.java
@@ -0,0 +1,18 @@
+// we have to convert the kilometer distance into miles
+
+public class DistanceConversion{
+	public static void main(String[] args){
+	
+	//define the kilometer
+		double kilometer= 10.8;  
+		
+	     
+		//define the miles and calculate it 1 km = 1.6 miles
+		double distanceOfMiles= 10.8*1.6 ;  
+		
+		//here we have to print the output of conversion of kilometer into miles 
+		System.out.println( " The distance 10.8 km in miles is " + distanceOfMiles);
+
+	}
+
+}
