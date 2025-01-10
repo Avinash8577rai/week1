86843d01fa7b16345c46186461c8323ef6d3363d
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-01-10 12:44:13 +0530

    CountdownUsingforloop_problem9

---
+++ Level1_control/CountdownUsingWhile.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class CountdownUsingWhile{
+    public static void main(String[] args) {
+		
+		//here we make a object input
+        Scanner input = new Scanner(System.in);
+
+        // Get the countdown start value from user
+        System.out.print("Enter the starting value for countdown - ");
+        int counter = input.nextInt();
+
+        // Countdown
+        while (counter >= 1) {
+            System.out.println(counter);
+            counter--; // Decrementing the counter
+        }
+         // close the input object
+        input.close();
+    }
+}
