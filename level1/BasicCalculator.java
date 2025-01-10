13d7ea3d660eae08278c6f754cb1ad4b4cc708e3
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-01-10 09:54:28 +0530

    BasicCalculator_Problem11

---
+++ level1/BasicCalculator.java
@@ -0,0 +1,28 @@
+// to program to create a basic calculator that performs addition, subtraction, multiplication, and division:
+import java.util.Scanner;
+
+public class BasicCalculator{
+    public static void main(String[] args) {
+	
+	//make object to take inp[ut from user
+	Scanner input=new Scanner(System.in);
+	
+	//take input of first number from user
+	System.out.print("Enter the first number ");
+	double number1 = input.nextDouble();
+	
+	//take input  of second number from user 
+	System.out.print("Enter the second number ");
+	double number2 = input.nextDouble();
+	
+	// Perform arithmetic operations
+	double addition = number1 + number2;
+	double subtraction= number1 - number2;
+	double multiplication= number1* number2;
+	double division= number1 / number2;
+	
+	System.out.println("The addition, subtraction, multiplication, and division value of two numbers "+ number1 +" and "+ number2 +" is "  + addition + " , " + subtraction + " , " + multiplication + " , and " + division );
+	     // Close the scanner
+        input.close();
+    }
+}
