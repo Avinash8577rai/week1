88e4b5a8d437a229c9d7d2d6bf81ea5cccea51c1
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-01-10 09:44:42 +0530

    Harry's Age_Problem1

---
+++ level1/HarryAge.java
@@ -0,0 +1,13 @@
+// here we take class name is HarryAge becuse we have to calculate the harry age
+public class HarryAge{
+public static void main (String[] arge) {
+int birthYear = 2000;   // define the birthYear
+int currentYear = 2024;  // define the currentYear
+
+// now we have to find the currentage of harry so we have to subtract the currentYear from birthYear
+int age= currentYear- birthYear;
+//now we print the out of the harray's age currently in 2024
+System.out.println("  Harry's age in 2024 is  " + age )
+
+}
+}
